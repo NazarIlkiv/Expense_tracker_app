1c11475a5cc6d744e585ecde6d6b8ff49bebd6fc
nazarilkiv <bruem35@gmail.com>
2024-04-23 22:55:26 +0300

    initialize app Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
+import 'package:bloc/bloc.dart';
 import 'package:expenses_tracker_app/app.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  Bloc.observer = SimpleBlocOnserver();
   runApp(const MyApp());
 }
