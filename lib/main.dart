4d6a01b6a435922fad401af5b15ed75ef8e51b01
nazarilkiv <bruem35@gmail.com>
2024-04-26 08:28:26 +0300

    create BLoC

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,12 @@
 import 'package:bloc/bloc.dart';
 import 'package:expenses_tracker_app/app.dart';
+import 'package:expenses_tracker_app/simple_bloc_observer.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  Bloc.observer = SimpleBlocOnserver();
+  Bloc.observer = SimpleBlocObserver();
   runApp(const MyApp());
 }
